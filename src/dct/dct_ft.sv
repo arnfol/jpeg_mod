8e22b6e448ab19fea264aacb8f3b382483f9d18f
Ali Allavi <a.allavi@gmail.com>
2019-06-16 15:17:52 +0300

    Поменял местами уменьшаемые и вычитаемые в строках 29,57, 58 + мелкие правки

--- src/dct/dct_ft.sv
+++ src/dct/dct_ft.sv
@@ -14,6 +14,7 @@ end
 logic signed [15:0] temp_s1x [7:0];
 
 always_comb begin : proc_stage_1
+   foreach (temp_s1x[i]) temp_s1x[i] = '0;
    for (int i = 0; i < 4; i++) begin
       temp_s1x[i]   = sx_in[i] + sx_in[7-i];
       temp_s1x[4+i] = sx_in[3-i] - sx_in[4+i];
@@ -21,10 +22,11 @@ always_comb begin : proc_stage_1
 end
 
 // stage 2
-logic signed [15:0] temp_s2x [6:5];
+logic signed [15:0] temp_s2x [7:0];
 
 always_comb begin : proc_stage_2
-   temp_s2x[5] = (temp_s2x[6]>>>3 + temp_s2x[6]>>>2 + temp_s2x[6]>>>2) - temp_s1x[5];
+   foreach (temp_s2x[i]) temp_s2x[i] = temp_s1x[i];
+   temp_s2x[5] = (temp_s2x[6]>>>3 + temp_s2x[6]>>>1) - temp_s1x[5];
    temp_s2x[6] = (temp_s1x[5]>>>3 + temp_s1x[5]>>>2) + temp_s1x[6];
 end
 
@@ -32,28 +34,29 @@ end
 logic signed [15:0] temp_s3x [7:0];
 
 always_comb begin : proc_stage_3
-   temp_s3x[0] = temp_s1x[0] + temp_s1x[3];
-   temp_s3x[1] = temp_s1x[1] + temp_s1x[2];
-   temp_s3x[2] = temp_s1x[1] - temp_s1x[2];
-   temp_s3x[3] = temp_s1x[0] - temp_s1x[3];
-   temp_s3x[4] = temp_s1x[4] + temp_s2x[5];
-   temp_s3x[5] = temp_s1x[4] - temp_s2x[5];
-   temp_s3x[6] = temp_s1x[7] - temp_s2x[6];
-   temp_s3x[7] = temp_s1x[7] + temp_s2x[6];
+   foreach (temp_s3x[i]) temp_s3x[i] = temp_s2x[i];
+   temp_s3x[0] = temp_s2x[0] + temp_s2x[3];
+   temp_s3x[1] = temp_s2x[1] + temp_s2x[2];
+   temp_s3x[2] = temp_s2x[1] - temp_s2x[2];
+   temp_s3x[3] = temp_s2x[0] - temp_s2x[3];
+   temp_s3x[4] = temp_s2x[4] + temp_s2x[5];
+   temp_s3x[5] = temp_s2x[4] - temp_s2x[5];
+   temp_s3x[6] = temp_s2x[7] - temp_s2x[6];
+   temp_s3x[7] = temp_s2x[7] + temp_s2x[6];
 end
 
 // stage 4
 logic signed [15:0] temp_s4x [7:0];
 
 always_comb begin : proc_stage_4
+   foreach (temp_s4x[i]) temp_s4x[i] = temp_s3x[i];
    temp_s4x[0] = temp_s3x[0] + temp_s3x[1];
    temp_s4x[1] = temp_s4x[0]>>>1 - temp_s3x[1];
    temp_s4x[2] = (temp_s3x[3]>>>3 + temp_s3x[3]>>>2) - temp_s3x[2];
    temp_s4x[3] = (temp_s4x[2]>>>3 + temp_s4x[2]>>>2) + temp_s3x[3];
-   temp_s4x[4] = temp_s3x[7]>>>3 - temp_s3x[4];
+   temp_s4x[4] = temp_s3x[4] - temp_s3x[7]>>>3;
    temp_s4x[5] = (temp_s3x[6]>>>3 + temp_s3x[6]>>>2 + temp_s3x[6]>>>1) + temp_s3x[5];
-   temp_s4x[6] = temp_s4x[5]>>>1 - temp_s3x[6];
-   temp_s4x[7] = temp_s3x[7];
+   temp_s4x[6] = temp_s3x[6] - temp_s4x[5]>>>1;
 end
 
 // output res
