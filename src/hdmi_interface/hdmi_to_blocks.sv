c98d3721cb96d74581a2d99773b0c57fc477dc03
arnfol <arnfol@mail.ru>
2019-06-24 23:01:24 +0300

    started working on control logic in hdmi interface

--- src/hdmi_interface/hdmi_to_blocks.sv
+++ src/hdmi_interface/hdmi_to_blocks.sv
@@ -27,7 +27,6 @@ module hdmi_to_blocks #(
 	localparam BUF_DEPTH = 2160*BLOCK_SIZE/N;
 
 	logic [8*3*N-1:0] buf1, buf2 [BUF_DEPTH];
-	logic                         buf1_en, buf2_en;
 	logic                         buf1_wr_en, buf2_wr_en;
 	logic [$clog2(BUF_DEPTH)-1:0] buf1_rd_addr, buf2_rd_addr;
 	logic [            8*3*N-1:0] buf1_o, buf2_o;
@@ -40,21 +39,27 @@ module hdmi_to_blocks #(
 	logic rd_cntr_clr;
 	logic rd_cntr_en;
 
+	integer block;
+	integer block_line;
+	integer block_elem;
+	logic buf_empty;
+
+	logic next_is_sof;
 
 	/*------------------------------------------------------------------------------
 	--  INPUT BUFFERS
 	------------------------------------------------------------------------------*/
 	always_ff @(posedge clk) begin
-		if(buf1_en) begin
-			buf1_o <= buf1[buf1_rd_addr];
-			if(buf1_wr_en) buf1[wr_cntr] <= {hdmi_data_cb, hdmi_data_cr, hdmi_data_y};
+		buf1_o <= buf1[buf1_rd_addr];
+		if(buf1_wr_en) begin
+			buf1[wr_cntr] <= {hdmi_data_cb, hdmi_data_cr, hdmi_data_y};
 		end
 	end
 
 	always_ff @(posedge clk) begin
-		if(buf2_en) begin
-			buf2_o <= buf2[buf2_rd_addr];
-			if(buf2_wr_en) buf2[wr_cntr] <= {hdmi_data_cb, hdmi_data_cr, hdmi_data_y};
+		buf2_o <= buf2[buf2_rd_addr];
+		if(buf2_wr_en) begin
+			buf2[wr_cntr] <= {hdmi_data_cb, hdmi_data_cr, hdmi_data_y};
 		end
 	end
 
@@ -65,34 +70,36 @@ module hdmi_to_blocks #(
 	always_ff @(posedge clk or negedge rst_n) begin : proc_wr_cntr
 		if(~rst_n) begin
 			wr_cntr <= '0;
-		end else if(wr_cntr_clr) begin
-			wr_cntr <= '0;
+			buf_full <= 0;
 		end else if(wr_cntr_en) begin
-			wr_cntr <= wr_cntr + 1;
+			if(wr_cntr < BUF_DEPTH) begin 
+				wr_cntr <= wr_cntr + 1;
+				buf_full <= (wr_cntr == BUF_DEPTH-2);
+			end else begin 
+				wr_cntr <= '0;
+			end
 		end
 	end	
 
-	integer block;
-	integer block_line;
-	integer block_elem;
 	always_ff @(posedge clk or negedge rst_n) begin 
 	 	if(~rst_n) begin
 	 		block <= '0;
 	 		block_line <= '0;
 	 		block_elem <= '0;
+	 		buf_empty <= 0;
 	 	end else if(rd_cntr_clr) begin
 			block <= '0;
 	 		block_line <= '0;
 	 		block_elem <= '0;
+	 		buf_empty <= 0;
 		end else if(rd_cntr_en)begin
+	 		buf_empty <= 0;
 	 		if(block_elem == BLOCK_SIZE/N-1) begin // end of block line
-		 		block_elem <= 0;
 		 		if(block_line == BLOCK_SIZE-1) begin // end of block
 		 			block_line <= 0;
 		 			block <= block + 1;
-		 			if(block == X_RES/BLOCK_SIZE-1) begin // end of buffer
-		 				
-		 			end
+		 			if(block == X_RES/BLOCK_SIZE-1) buf_empty <= 1;
+		 			else block_elem <= 0;
 	 			// if not end of block
 		 		end else block_line <= block_line + 1;
 		 	// if not end of block line
@@ -103,24 +110,56 @@ module hdmi_to_blocks #(
 	assign rd_cntr = block_elem + block_line*X_RES/N + block*BLOCK_SIZE/N;
 
 	/*------------------------------------------------------------------------------
-	--  FSM
+	--  Control logic
 	------------------------------------------------------------------------------*/
-	assign blk_eob = (block_elem == BLOCK_SIZE/N-1) && (block_line == BLOCK_SIZE-1);
-	assign blk_sob = (block_elem == 0) && (block_line == 0);
+
+	// Remember start of frame
+	always_ff @(posedge clk or negedge rst_n) begin 
+		if(~rst_n) begin
+			next_is_sof <= 0;
+		end else begin
+			if(hdmi_v_sync) next_is_sof <= 1;
+			else if(blk_sof) next_is_sof <= 0;
+		end
+	end
 
 	// TODO: make a FSM
+	// always_ff @(posedge clk or negedge rst_n) begin 
+	// 	if(~rst_n) begin
+	// 		rd_cntr_clr <= 1;
+	// 		wr_cntr_clr <= 1;
+	// 	end else begin
+	// 		 <= ;
+	// 	end
+	// end
 	assign rd_cntr_clr = 0;
-	assign rd_cntr_en  = 1;
 	assign wr_cntr_clr = 0;
-	assign wr_cntr_en = 1;
-	assign buf1_en = 1;
-	assign buf2_en = 1;
+	assign rd_cntr_en  = 1;
+	assign wr_cntr_en = buf1_wr_en || buf2_wr_en;
+	assign buf1_wr_en = 1 && hdmi_data_valid;
+	assign buf2_wr_en = 1 && hdmi_data_valid;
 
-	
-
-	// TODO: add pipeline here
-	assign blk_data_y  = (1) ? buf2_o[8*N-1   : 0]     : buf1_o[8*N-1   : 0]    ;
-	assign blk_data_cr = (1) ? buf2_o[8*N*2-1 : 8*N]   : buf1_o[8*N*2-1 : 8*N]  ;
-	assign blk_data_cb = (1) ? buf2_o[8*N*3-1 : 8*N*2] : buf1_o[8*N*3-1 : 8*N*2];
+	/*------------------------------------------------------------------------------
+	--  Output pipeline
+	------------------------------------------------------------------------------*/
+	always_ff @(posedge clk or negedge rst_n) begin 
+		if(~rst_n) begin
+			blk_data_y  <= '0;
+			blk_data_cr <= '0;
+			blk_data_cb <= '0;
+			blk_valid <= 0;
+			blk_eob <= 0;
+			blk_sob <= 0;
+			blk_sof <= 0;
+		end else begin
+			blk_data_y  <= (1) ? buf2_o[8*N-1   : 0]     : buf1_o[8*N-1   : 0]    ;
+			blk_data_cr <= (1) ? buf2_o[8*N*2-1 : 8*N]   : buf1_o[8*N*2-1 : 8*N]  ;
+			blk_data_cb <= (1) ? buf2_o[8*N*3-1 : 8*N*2] : buf1_o[8*N*3-1 : 8*N*2];
+			blk_valid <= 0; // TODO: add logic here
+			blk_eob <= (block_elem == BLOCK_SIZE/N-1) && (block_line == BLOCK_SIZE-1);
+			blk_sob <= (block_elem == 0) && (block_line == 0);
+			blk_sof <= (block_elem == 0) && (block_line == 0) && next_is_sof;
+		end
+	end
 
 endmodule
